7e2d58deaf2a105cf63c0ea45279f372f2cdb0e7
Ram ( Ramudu Kondapalli) <ramudu.kp@gmail.com>
2020-07-04 10:52:18 -0400

    Add files via upload

---
+++ src/app/item-output/item-output.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Output, EventEmitter  } from '@angular/core';
+
+@Component({
+  selector: 'app-item-output',
+  templateUrl: './item-output.component.html',
+  styleUrls: ['./item-output.component.css']
+})
+export class ItemOutputComponent implements OnInit {
+
+  
+  constructor() { }
+
+  ngOnInit(): void { }
+  
+}
